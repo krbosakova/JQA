777c21a9445e1f8d870b7c0d87807ca8da50b5e6
krbosakova <49039376+krbosakova@users.noreply.github.com>
2023-04-10 14:34:40 +0300

    Update FirstTestCase.java

--- src/Homework8/FirstTestCase.java
+++ src/Homework8/FirstTestCase.java
@@ -3,7 +3,7 @@ package Homework8;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver; 
 
 public class FirstTestCase {
     public static void main(String[] args) {
