159e1602d3a9d7b410cca5387ae94d9be0000e60
krbosakova <49039376+krbosakova@users.noreply.github.com>
2023-04-10 14:33:48 +0300

    Add files via upload

---
+++ src/Homework8/FirstTestCase.java
@@ -0,0 +1,25 @@
+
+package Homework8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestCase {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bosakova\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://www.epay.bg/v3main/front");
+
+        driver.findElement(By.id("login_user")).sendKeys("yvonnej");
+        driver.findElement(By.id("login_pass")).sendKeys("1223456");
+        driver.findElement(By.id("login_submit")).click();
+
+        String sampleText = driver.findElement(By.className("login_err_msg")).getText();
+        System.out.println(sampleText);
+
+        driver.quit();
+    }
+}
